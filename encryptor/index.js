734a8170d3ef98336e3cc3e9dc3789d2f8ae287d
ShreksGodSon8 <128639192+ShreksGodSon8@users.noreply.github.com>
2023-05-03 13:00:32 -0700

    Update index.js

--- encryptor/index.js
+++ encryptor/index.js
@@ -1,1 +1,10 @@
 console.log('The encryptor js file loaded'); 
+
+const letters = 'abcdefghijklmnopqrstuvwxyz';
+const encrypt_key = 'bcdefghijklmnopqrstuvwxyza';
+
+console.log(letters[0]); // a
+
+for(let i = 0; i < letters.length; i++) {
+   console.log(letters[i]);
+}
