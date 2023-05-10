6153929354144dce292574649162c1e3425d42dd
ShreksGodSon8 <128639192+ShreksGodSon8@users.noreply.github.com>
2023-05-10 12:15:32 -0700

    Update index.js

--- encryptor/index.js
+++ encryptor/index.js
@@ -1,9 +1,13 @@
 console.log('The encryptor js file loaded'); 
 
-const letters = 'abcdefghijklmnopqrstuvwxyz';
+const letters =     'abcdefghijklmnopqrstuvwxyz';
 const encrypt_key = 'bcdefghijklmnopqrstuvwxyza';
 
 console.log(letters[0]); // a
+
+for (let i = 0; i < 10; i++) {
+   console.log('loop: ' +i);
+}
 
 for(let i = 0; i < letters.length; i++) {
    console.log(letters[i]);
