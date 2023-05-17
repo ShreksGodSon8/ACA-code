1ac7a8a49651f71923933f12934f2576e6760ea7
ShreksGodSon8 <128639192+ShreksGodSon8@users.noreply.github.com>
2023-05-17 12:40:19 -0700

    Update index.js

--- encryptor/index.js
+++ encryptor/index.js
@@ -36,8 +36,27 @@ fUnction decrypt(text) {
 
   else {
      result += letters[letterIndex];
-    )  
- } 
-   
+  }// else
+      
+ }// for 
 return result;
-   }      
+      
+   }// decrypt
+   
+   // add event listener to the form
+   const encryptForm = document.getElementById('encrypt-form')
+   encryptForm.addEventlistener('submit',function(e) {
+     e.preventDefault(); 
+      
+      // get the value of the content
+     let messageElement = document.getElementById('encrypt');
+     let message = messageElement.value;
+      
+      // encrypt the content
+     let encrypted = encrypt(message);
+      
+      // display encrypted message
+      let encodedMessageElement = document.getElementById('encoded-message');
+      encodedMessageElement.innerText = encrypted;
+   });
+   
